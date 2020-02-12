ce7be1341e9454ab951da164fe809896f416db28
RamiKrispin <ramkrisp@umich.edu>
2020-02-11 23:27:24 -0800

    Update the data pull

--- data_raw/pulling raw data.R
+++ data_raw/pulling raw data.R
@@ -15,7 +15,7 @@ for(i in 5:ncol(raw_conf)){
   raw_conf[,i] <- ifelse(is.na(raw_conf[, i]), 0 , raw_conf[, i])
 
   if(i == 5){
-  df_conf[[names(raw_conf)[i]]] <- raw_conf[, i]
+    df_conf[[names(raw_conf)[i]]] <- raw_conf[, i]
   } else {
     df_conf[[names(raw_conf)[i]]] <- raw_conf[, i] - raw_conf[, i - 1]
   }
@@ -24,10 +24,21 @@ for(i in 5:ncol(raw_conf)){
 
 df_conf1 <-  df_conf %>% tidyr::pivot_longer(cols = dplyr::starts_with("X"),
                                              names_to = "date_temp",
-                                             values_to = "cases")
+                                             values_to = "cases_temp")
 
-df_conf1$month <- strsplit(df_conf1$date_temp[1], split = "\\.")
+df_conf1$month <- sub("X", "",
+                      strsplit(df_conf1$date_temp, split = "\\.") %>%
+                        purrr::map_chr(~.x[1]) )
+
+df_conf1$day <- strsplit(df_conf1$date_temp, split = "\\.") %>%
+  purrr::map_chr(~.x[2])
 
 
+df_conf1$date <- as.Date(paste("2020", df_conf1$month, df_conf1$day, sep = "-"))
 
-head(df_conf1)
+
+df_conf2 <- df_conf1 %>%
+  dplyr::group_by(Province.State, Country.Region, Lat, Long, date) %>%
+  dplyr::summarise(cases = sum(cases_temp))
+head(df_conf2)
+tail(df_conf2)
