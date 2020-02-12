a11b4197911d8232d2e8232cb7edc24ed1ab48bb
RamiKrispin <ramkrisp@umich.edu>
2020-02-12 00:00:57 -0800

    Update the data pull file

--- data_raw/pulling raw data.R
+++ data_raw/pulling raw data.R
@@ -1,13 +1,17 @@
+#----------------------------------------------------
+# Pulling the coronvirus data from John Hopkins repo
+# https://github.com/CSSEGISandData/COVID-19
+#----------------------------------------------------
+# Setting functions
 `%>%` <- magrittr::`%>%`
+#----------------------------------------------------
+# Pulling confirmed cases
 
 raw_conf <- read.csv(file = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/time_series/time_series_2019-ncov-Confirmed.csv",
                      stringsAsFactors = FALSE)
-
-str(raw_conf)
-
+# Transforming the data from wide to long
+# Creating new data frame
 df_conf <- raw_conf[, 1:4]
-
-head(df_conf)
 
 for(i in 5:ncol(raw_conf)){
   print(i)
@@ -26,6 +30,7 @@ df_conf1 <-  df_conf %>% tidyr::pivot_longer(cols = dplyr::starts_with("X"),
                                              names_to = "date_temp",
                                              values_to = "cases_temp")
 
+# Parsing the date
 df_conf1$month <- sub("X", "",
                       strsplit(df_conf1$date_temp, split = "\\.") %>%
                         purrr::map_chr(~.x[1]) )
@@ -36,9 +41,108 @@ df_conf1$day <- strsplit(df_conf1$date_temp, split = "\\.") %>%
 
 df_conf1$date <- as.Date(paste("2020", df_conf1$month, df_conf1$day, sep = "-"))
 
-
+# Aggregate the data to daily
 df_conf2 <- df_conf1 %>%
   dplyr::group_by(Province.State, Country.Region, Lat, Long, date) %>%
-  dplyr::summarise(cases = sum(cases_temp))
+  dplyr::summarise(cases = sum(cases_temp)) %>%
+  dplyr::mutate(type = "confirmed")
+
 head(df_conf2)
 tail(df_conf2)
+#----------------------------------------------------
+# Pulling death cases
+
+raw_death <- read.csv(file = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/time_series/time_series_2019-ncov-Deaths.csv",
+                      stringsAsFactors = FALSE)
+# Transforming the data from wide to long
+# Creating new data frame
+df_death <- raw_death[, 1:4]
+
+for(i in 5:ncol(raw_death)){
+  print(i)
+  raw_death[,i] <- as.integer(raw_death[,i])
+  raw_death[,i] <- ifelse(is.na(raw_death[, i]), 0 , raw_death[, i])
+
+  if(i == 5){
+    df_death[[names(raw_death)[i]]] <- raw_death[, i]
+  } else {
+    df_death[[names(raw_death)[i]]] <- raw_death[, i] - raw_death[, i - 1]
+  }
+}
+
+
+df_death1 <-  df_death %>% tidyr::pivot_longer(cols = dplyr::starts_with("X"),
+                                               names_to = "date_temp",
+                                               values_to = "cases_temp")
+
+# Parsing the date
+df_death1$month <- sub("X", "",
+                       strsplit(df_death1$date_temp, split = "\\.") %>%
+                         purrr::map_chr(~.x[1]) )
+
+df_death1$day <- strsplit(df_death1$date_temp, split = "\\.") %>%
+  purrr::map_chr(~.x[2])
+
+
+df_death1$date <- as.Date(paste("2020", df_death1$month, df_death1$day, sep = "-"))
+
+# Aggregate the data to daily
+df_death2 <- df_death1 %>%
+  dplyr::group_by(Province.State, Country.Region, Lat, Long, date) %>%
+  dplyr::summarise(cases = sum(cases_temp)) %>%
+  dplyr::mutate(type = "death")
+
+head(df_death2)
+tail(df_death2)
+#----------------------------------------------------
+# Pulling recovered cases
+
+raw_rec <- read.csv(file = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/time_series/time_series_2019-ncov-Recovered.csv",
+                    stringsAsFactors = FALSE)
+# Transforming the data from wide to long
+# Creating new data frame
+df_rec <- raw_rec[, 1:4]
+
+for(i in 5:ncol(raw_rec)){
+  print(i)
+  raw_rec[,i] <- as.integer(raw_rec[,i])
+  raw_rec[,i] <- ifelse(is.na(raw_rec[, i]), 0 , raw_rec[, i])
+
+  if(i == 5){
+    df_rec[[names(raw_rec)[i]]] <- raw_rec[, i]
+  } else {
+    df_rec[[names(raw_rec)[i]]] <- raw_rec[, i] - raw_rec[, i - 1]
+  }
+}
+
+
+df_rec1 <-  df_rec %>% tidyr::pivot_longer(cols = dplyr::starts_with("X"),
+                                           names_to = "date_temp",
+                                           values_to = "cases_temp")
+
+# Parsing the date
+df_rec1$month <- sub("X", "",
+                     strsplit(df_rec1$date_temp, split = "\\.") %>%
+                       purrr::map_chr(~.x[1]) )
+
+df_rec1$day <- strsplit(df_rec1$date_temp, split = "\\.") %>%
+  purrr::map_chr(~.x[2])
+
+
+df_rec1$date <- as.Date(paste("2020", df_rec1$month, df_rec1$day, sep = "-"))
+
+# Aggregate the data to daily
+df_rec2 <- df_rec1 %>%
+  dplyr::group_by(Province.State, Country.Region, Lat, Long, date) %>%
+  dplyr::summarise(cases = sum(cases_temp)) %>%
+  dplyr::mutate(type = "recovered")
+
+head(df_rec2)
+tail(df_rec2)
+
+coronvirus <- dplyr::bind_rows(df_conf2, df_death2, df_rec2) %>%
+  dplyr::arrange(date)
+head(coronvirus)
+tail(coronvirus)
+
+usethis::use_data(coronvirus, overwrite = TRUE)
