a224086ea4b3d38d9c9eaf60ed20a9511afb690b
RamiKrispin <ramkrisp@umich.edu>
2020-02-12 23:24:30 -0800

    Add tests

---
+++ tests/testthat/test-data_valid.R
@@ -0,0 +1,12 @@
+test_that(desc = "Test the dates", {
+  expect_equal(base::class(coronavirus$date) == "Date", TRUE)
+  expect_equal(base::min(coronavirus$date) == as.Date("2020-01-21"), TRUE)
+})
+
+
+test_that(desc = "Test the type variable", {
+  expect_equal(base::all(c("confirmed", "death", "recovered") %in%
+                           unique(coronavirus$type)), TRUE)
+  expect_equal(base::any(is.na(coronavirus$type)), FALSE)
+})
+
