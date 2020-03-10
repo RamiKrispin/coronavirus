fee27ddf75e0e81d21766a095696deaed92278c7
RamiKrispin <ramkrisp@umich.edu>
2020-03-10 00:06:46 -0700

    Add SK data

--- data_raw/scripting_wiki.R
+++ data_raw/scripting_wiki.R
@@ -50,11 +50,113 @@ url_sk <-  "https://en.wikipedia.org/wiki/2020_coronavirus_outbreak_in_South_Kor
 
 sk_raw <- url_sk %>%
   xml2::read_html() %>%
-  rvest::html_node(xpath = '//*[@id="mw-content-text"]/div/table[7]') %>%
+  rvest::html_node(xpath = '//*[@id="mw-content-text"]/div/table[8]') %>%
   rvest::html_table(fill = TRUE,
              header = TRUE)
 
+head(sk_raw)
+sk_prov_map <- data.frame(province = c("Gyeonggi", "Gyeonggi", "Gyeonggi",
+                                       "Gangwon",
+                                       "Gyeongsang", "Gyeongsang", "Gyeongsang", "Gyeongsang","Gyeongsang", "Gyeongsang",
+                                       "Chungcheong", "Chungcheong", "Chungcheong", "Chungcheong",
+                                       "Jeolla", "Jeolla", "Jeolla", "Jeolla"),
+                          city = c("Incheon", "Seoul", "Gyeonggi",
+                                   "Gangwon",
+                                   "Daegu", "Gyeongbuk","Gyeongnam", "Gyeongsang", "Busan", "Ulsan",
+                                   "Chungbuk", "Chungnam", "Sejong", "Daejeon",
+                                   "Jeonbuk", "Jeonnam", "Gwangju", "Jeju"),
+                          stringsAsFactors = FALSE)
 
+sk_prov_map$province <- tolower(sk_prov_map$province)
+sk_prov_map$city <- tolower(sk_prov_map$city)
+
+sk_names <- sk_raw[1, ] %>% as.character() %>% tolower()
+sk_names <- sk_names[- which(sk_names == "gyeongsang")]
+sk_names[20] <- "confirmed_new"
+sk_names[21] <- "confirmed_total"
+sk_names[22] <- "death_new"
+sk_names[23] <- "death_total"
+sk_names[24] <- "tested_total"
+sk_names[25] <- "tested_current"
+sk_names[26] <- "discharged_total"
+sk_names <- c(sk_names, "source")
+
+
+sk_df <- sk_raw[-1,] %>% stats::setNames(sk_names) %>%
+  dplyr::select(-time, -source) %>%
+  dplyr::mutate(date = lubridate::ymd(date)) %>%
+  dplyr::filter(!is.na(date))
+
+
+head(sk_df)
+tail(sk_df)
+str(sk_df)
+
+sk_df1 <- sk_df %>% dplyr::select(date)
+# Removing brackets
+for(i in 2:ncol(sk_df)){
+  x <- ifelse(grepl(")", x = sk_df[,i]),strsplit(sk_df[, i], split = ")") %>% purrr::map_chr(~.x[2]), sk_df[, i] )
+  x <- ifelse(grepl("\\[", x = x),strsplit(x, split = "\\[") %>% purrr::map_chr(~.x[1]), x )
+  x <- ifelse(grepl(",", x = x),strsplit(x, split = "\\[") %>% purrr::map_chr(~.x[1]), x )
+  x <- gsub(pattern = ",", replacement = "", x)
+  sk_df1[[names(sk_df)[i]]] <- x
+}
+
+View(sk_df1)
+
+
+sk_df2 <- sk_df1 %>% tidyr::pivot_longer(cols = c(-date), names_to = "city") %>%
+  dplyr::mutate(cases = as.numeric(value)) %>%
+  dplyr::mutate(cases = ifelse(is.na(cases), 0, cases))
+View(sk_df2)
+
+totals <- c("confirmed_new", "confirmed_total",
+            "death_new", "death_total",
+            "tested_total", "tested_current",
+            "discharged_total")
+
+sk_df3 <- sk_df2 %>% dplyr::filter(city %in% totals) %>%
+  dplyr::group_by(date, city) %>%
+  dplyr::summarise(total = max(cases, na.rm = TRUE)) %>%
+  dplyr::ungroup()
+
+View(sk_df3)
+
+sk_df4 <- sk_df2 %>% dplyr::filter(!city %in% totals) %>%
+  dplyr::group_by(date, city) %>%
+  dplyr::summarise(total = sum(cases, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::select(date, type = city, total)
+
+View(sk_df4)
+
+#### Need to
+# split non city categories and take max per day
+# for cities calculate the daily sum
+
+
+
+
+
+#----------------Germany----------------
+# Summarise table of cases in the Germany
+# Using : https://en.wikipedia.org/wiki/2020_coronavirus_outbreak_in_Germany
+
+url_gr <-  "https://en.wikipedia.org/wiki/2020_coronavirus_outbreak_in_Germany"
+
+gr_raw <- url_gr %>%
+  xml2::read_html() %>%
+  rvest::html_node(xpath = '//*[@id="mw-content-text"]/div/table[3]') %>%
+  rvest::html_table(fill = TRUE,
+                    header = TRUE) %>% t
+
+
+gr_names <- c("date_temp", gr_raw[1, 2:ncol(gr_raw)])
+
+gr_raw2 <- gr_raw[-1, ] %>% as.data.frame() %>%
+  setNames(gr_names) %>%
+  dplyr::add_rownames(var = "month")
+View(gr_raw2)
 sk_prov_map <- data.frame(province = c("Gyeonggi", "Gyeonggi", "Gyeonggi",
                                        "Gangwon",
                                        "Gyeongsang", "Gyeongsang", "Gyeongsang", "Gyeongsang","Gyeongsang", "Gyeongsang",
