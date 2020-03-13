52a2d744c0268479c12b85ecabb8a50c290695e0
RamiKrispin <ramkrisp@umich.edu>
2020-03-13 00:44:22 -0700

    Updating code for pulling iran

--- data_raw/scripting_wiki.R
+++ data_raw/scripting_wiki.R
@@ -243,6 +243,64 @@ write.csv(italy2, "/Users/ramikrispin/R/packages/coronavirus_csv/italy/covid_ita
 write.csv(italy_region_mapping, "/Users/ramikrispin/R/packages/coronavirus_csv/italy/italy_region_mapping.csv", row.names = FALSE)
 
 
+
+
+
+#----------------Iran----------------
+# Summarise table of cases in the Iran
+# Using : https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data/Iran_medical_cases
+
+url_iran <-  "https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data/Iran_medical_cases"
+
+iran_raw <- url_iran %>%
+  xml2::read_html() %>%
+  rvest::html_node(xpath = '//*[@id="mw-content-text"]/div/table[2]') %>%
+  rvest::html_table(fill = TRUE,
+                    header = TRUE)
+
+
+iran_region_mapping <- data.frame(region = c(rep("Region 1", 7), rep("Region 2", 6),
+                                             rep("Region 3", 6), rep("Region 4", 6),
+                                             rep("Region 5", 6)) ,
+                                  province = c("Qom", "Teh", "Maz", "Alb", "Sem", "Gol", "Qaz", "Esf", "Frs", "Hor", "Koh", "Cha", "Bus",
+                                               "Gil", "Ard", "Azs", "Azg", "Kur", "Zan", "Mar", "Ham", "Khz", "Krs", "Lor", "Ilm", "Khr",
+                                               "Sis", "Yaz", "Khs", "Ker", "Khn"),
+                                  stringsAsFactors = FALSE)
+
+iran_names <- iran_raw[1,] %>% as.character()
+iran_names[33] <- "confirmed_new"
+iran_names[34] <- "confirmed_total"
+iran_names[35] <- "death_new"
+iran_names[36] <- "death_total"
+
+
+
+covid_iran<- iran_raw[-1, ] %>% stats::setNames(iran_names) %>%
+  dplyr::select(- Sources) %>%
+  dplyr::mutate(date = as.Date(Date)) %>%
+  dplyr::select(-Date, -confirmed_new, -confirmed_total, -death_new, -death_total) %>%
+  dplyr::filter(!is.na(date)) %>%
+  tidyr::pivot_longer(cols = -date, names_to = "province") %>%
+  dplyr::mutate(cases = as.numeric(value)) %>%
+  dplyr::mutate(cases = ifelse(is.na(cases), 0, cases)) %>%
+  dplyr::select(-value) %>%
+  dplyr::left_join(iran_region_mapping, by = "province") %>%
+  dplyr::select(date, region, province, cases) %>%
+  as.data.frame()
+
+covid_iran_wide <- covid_iran %>% tidyr::pivot_wider(names_from = province, values_from = cases)
+
+str(covid_iran)
+View(covid_iran)
+
+usethis::use_data(covid_iran, overwrite = TRUE)
+
+write.csv(covid_iran, "/Users/ramikrispin/R/packages/coronavirus_csv/iran/covid_iran_long.csv", row.names = FALSE)
+write.csv(covid_iran_wide, "/Users/ramikrispin/R/packages/coronavirus_csv/iran/covid_iran_wide.csv", row.names = FALSE)
+write.csv(iran_region_mapping, "/Users/ramikrispin/R/packages/coronavirus_csv/iran/iran_region_mapping.csv", row.names = FALSE)
+
+
+
 #----------------Germany----------------
 # Summarise table of cases in the Germany
 # Using : https://en.wikipedia.org/wiki/2020_coronavirus_outbreak_in_Germany
