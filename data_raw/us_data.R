257141a5357d49a986cf4131af67a71d85ed80d5
RamiKrispin <ramkrisp@umich.edu>
2020-03-23 21:02:07 -0700

    Us data testing

--- data_raw/us_data.R
+++ data_raw/us_data.R
@@ -3,3 +3,7 @@
 
 df <- read.csv("https://covidtracking.com/api/states.csv", stringsAsFactors = FALSE)
 df
+
+
+df <- read.csv("https://coronadatascraper.com/#timeseries.csv", stringsAsFactors = FALSE)
+View(df)
