198c0cf6e0ee8efd2bbc5badc1bec7fc95931fb1
RamiKrispin <ramkrisp@umich.edu>
2020-04-01 22:09:22 -0700

    removing unused script

--- data_raw/scripting_wiki.R
+++ data_raw/scripting_wiki.R
@@ -2,55 +2,14 @@
 # Scripting different tables from wiki pages about coronavirus
 #----------------Functions----------------
 `%>%` <- magrittr::`%>%`
-#----------------US----------------
-# Summarise table of cases in the US
-# Using : https://en.wikipedia.org/wiki/2020_coronavirus_outbreak_in_the_United_States
-
-url <-  "https://en.wikipedia.org/wiki/2020_coronavirus_outbreak_in_the_United_States"
-
-us_raw <- url %>%
-  xml2::read_html() %>%
-  rvest::html_node(xpath = '//*[@id="mw-content-text"]/div/table[6]') %>%
-  rvest::html_table(fill = TRUE,
-                    header = TRUE)
-
-
-# Setting the names
-names(us_raw)[min(which(names(us_raw) == "Location"))] <- "county_city"
-names(us_raw)[min(which(names(us_raw) == "Location"))] <- "state"
-us_raw$date <- lubridate::mdy(us_raw$`Date announced`)
-
-# Dropping details raw
-us <- us_raw %>% dplyr::filter(!is.na(date)) %>%
-  dplyr::select(- `Date announced`) %>%
-  dplyr::select(case_no_temp = `Case no.`,
-                date,
-                status = Status,
-                cdc_origin_type = `CDC origin type`,
-                origin = Origin,
-                county_city,
-                state,
-                treatment_facility = `Treatment facility`,
-                sex = Sex,
-                age = Age) %>%
-  dplyr::mutate(case_no = strsplit(case_no_temp, split = "\\[") %>%
-                  purrr::map(~.x[1]) %>%
-                  as.numeric()) %>%
-  dplyr::select(-case_no_temp) %>%
-  dplyr::select(case_no, dplyr::everything())
-head(us)
-tail(us)
-str(us)
-
 #----------------South Korea----------------
 # Summarise table of cases in the South Korea
 # Using : https://en.wikipedia.org/wiki/2020_coronavirus_outbreak_in_South_Korea
-`%>%` <- magrittr::`%>%`
 url_sk <-  "https://en.wikipedia.org/wiki/2020_coronavirus_outbreak_in_South_Korea"
 
 sk_raw <- url_sk %>%
   xml2::read_html() %>%
-  rvest::html_node(xpath = '//*[@id="mw-content-text"]/div/table[8]') %>%
+  rvest::html_node(xpath = '//*[@id="mw-content-text"]/div/table[10]') %>%
   rvest::html_table(fill = TRUE,
                     header = TRUE)
 
