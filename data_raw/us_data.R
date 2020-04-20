56423abfd17dbc4e13a1cbf79118f525e89c6fa8
RamiKrispin <ramkrisp@umich.edu>
2020-04-19 20:01:54 -0700

    cleaning the files

--- data_raw/us_data.R
+++
@@ -1,9 +0,0 @@
-
-
-
-df <- read.csv("https://covidtracking.com/api/states.csv", stringsAsFactors = FALSE)
-df
-
-
-df <- read.csv("https://coronadatascraper.com/#timeseries.csv", stringsAsFactors = FALSE)
-View(df)
