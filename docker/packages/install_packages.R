ac80992266c8958f1accda6815faafdee8ed7442
RamiKrispin <ramkrisp@umich.edu>
2020-07-12 21:39:44 -0700

    update the required packages

--- docker/packages/install_packages.R
+++ docker/packages/install_packages.R
@@ -2,19 +2,11 @@
 pkg_list <- c("dplyr",
               "tibble",
               "devtools",
-              "fs",
-              "gh",
-              "glue",
               "here",
               "lubridate",
               "magrittr",
               "purrr",
-              "readr",
-              "snakecase",
-              "stringr",
-              "tibble",
               "rmarkdown",
-              "shiny",
               "tidyr",
               "testthat",
               "usethis")
@@ -22,17 +14,9 @@ pkg_list <- c("dplyr",
 install.packages(pkgs = pkg_list, repos = "https://cran.rstudio.com/")
 
 for(i in pkg_list){
-  
+
   if(!i %in% rownames(installed.packages())){
     stop(paste("Package", i, "is not available"))
   }
 }
 
-# install Covid19R project packages
-packages_list <- utils::read.csv("https://raw.githubusercontent.com/Covid19R/covid19Rdata/master/data-raw/packages.csv", 
-                                 stringsAsFactors = FALSE) 
-args <-paste(packages_list$username, packages_list$package, sep = "/")
-
-for (a in args) {
-  devtools::install_github(a, upgrade = "never")
-}
