edd1470bf2b9ba67778461a06d73dba0a5ffeab4
RamiKrispin <ramkrisp@umich.edu>
2021-08-21 21:56:41 -0700

    add tests for the vaccine dataset

--- tests/testthat/test-data_valid.R
+++ tests/testthat/test-data_valid.R
@@ -1,12 +1,19 @@
 test_that(desc = "Test the dates", {
-  expect_equal(base::class(coronavirus$date) == "Date", TRUE)
-  expect_equal(base::min(coronavirus$date) == as.Date("2020-01-22"), TRUE)
+  expect_equal(class(coronavirus$date) == "Date", TRUE)
+  expect_equal(min(coronavirus$date) == as.Date("2020-01-22"), TRUE)
 })
 
 
 test_that(desc = "Test the type variable", {
-  expect_equal(base::all(c("confirmed", "death") %in%
+  expect_equal(all(c("confirmed", "death") %in%
                            unique(coronavirus$type)), TRUE)
-  expect_equal(base::any(is.na(coronavirus$type)), FALSE)
+  expect_equal(any(is.na(coronavirus$type)), FALSE)
+})
+
+
+test_that(desc = "Test the vaccine data", {
+  expect_equal(any(is.na(covid19_vaccine$country_region)), FALSE)
+  expect_equal(class(covid19_vaccine$date) == "Date", TRUE)
+  expect_equal(class(covid19_vaccine$report_date_string) == "Date", TRUE)
 })
 
