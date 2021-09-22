b08f864b1bd1fc4a00a119b01910db44d0426334
RamiKrispin <ramkrisp@umich.edu>
2021-09-22 05:18:50 -0700

    add gis codes for the coronavirues datasset

--- data_raw/gis_mapping_data.R
+++ data_raw/gis_mapping_data.R
@@ -1,6 +1,6 @@
 # UID/ISO/FIPS mapping
-
-gis_code_mapping <- NULL
+`%>%` <- magrittr::`%>%`
+gis_code_mapping <- gis_codes_coronavirues <- NULL
 tryCatch(
   gis_code_mapping <- readr::read_csv(file = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv",
                              col_types = readr::cols(UID = readr::col_number(),
@@ -16,6 +16,16 @@ names(gis_code_mapping)[which(names(gis_code_mapping) == "long_")] <- "long"
 
 head(gis_code_mapping)
 
+
+
+gis_codes_coronavirues <- readr::read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv",
+                             col_types = readr::cols(FIPS = readr::col_number(),
+                                                     Admin2 = readr::col_character()))
+
+names(gis_codes_coronavirues) <- tolower(names(gis_codes_coronavirues))
+names(gis_codes_coronavirues)[which(names(gis_codes_coronavirues) == "long_")] <- "long"
+head(gis_codes_coronavirues)
+
 # Continent mapping
 continent_mapping <- readr::read_csv(file = "https://pkgstore.datahub.io/JohnSnowLabs/country-and-continent-codes-list/country-and-continent-codes-list-csv_csv/data/b7876b7f496677669644f3d1069d3121/country-and-continent-codes-list-csv_csv.csv",
     na = "null") %>%
@@ -27,4 +37,4 @@ continent_mapping <- readr::read_csv(file = "https://pkgstore.datahub.io/JohnSno
                 uid = Country_Number) %>% as.data.frame()
 head(continent_mapping)
 
-save(continent_mapping, gis_code_mapping, file = "./data_raw/gis_mapping.RData")
+save(continent_mapping, gis_code_mapping, gis_codes_coronavirues, file = "./data_raw/gis_mapping.RData")
