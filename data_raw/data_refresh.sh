245de562f389f7146ae22a477585ecee8c8edef2
RamiKrispin <ramkrisp@umich.edu>
2021-09-30 21:44:02 -0700

    update the refresh function

--- data_raw/data_refresh.sh
+++ data_raw/data_refresh.sh
@@ -3,7 +3,7 @@
 echo "Updating the total dataset"
 
  Rscript -e "source('./data_raw/data_refresh.R');
-             data_refresh(env = '$1');
+             rmarkdown::render(input = './data_pipelines/covid19_cases.Rmd', knit_root_dir = ".", output_dir = './docs/data_pipelines');
              data_refresh_vaccine(url = 'https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv',
                                   env = '$1');"
 
