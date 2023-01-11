6435645ea63b107e8bed1efa0fb47a5dae3f2854
rkrispin <rkrispin@apple.com>
2023-01-10 20:21:54 -0800

    update the docker files

--- docker/dev/build_dev.sh
+++ docker/dev/build_dev.sh
@@ -2,11 +2,16 @@
 
 echo "Build the docker"
 
-docker build . -t docker.io/rkrispin/coronavirus:dev.0.3.30.9000
+docker build . --progress=plain \
+               --build-arg QUARTO_VERSION=1.2.313 \
+               -t rkrispin/coronavirus:dev.0.3.34
+
+
+
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
-docker push docker.io/rkrispin/coronavirus:dev.0.3.30.9000
+docker push rkrispin/coronavirus:dev.0.3.34
 else
 echo "Docker build failed"
 fi
