9073df99e92087c94bda24ad8007c8d4b6a25f12
rkrispin <rkrispin@apple.com>
2023-01-13 09:40:29 -0800

    fix error with the folder path

--- data_raw/data_refresh.sh
+++ data_raw/data_refresh.sh
@@ -7,7 +7,7 @@ cp ./data_pipelines/covid19_cases.html ./docs/data_pipelines/
 cp -r ./data_pipelines/covid19_cases_files/ ./docs/data_pipelines/
 quarto render ./data_pipelines/vaccine_data.qmd --to html 
 cp ./data_pipelines/vaccine_data.html ./docs/data_pipelines/vaccine_data.html
-cp -r ./data_pipelines/vaccine_data_files/*.* ./docs/data_pipelines/vaccine_data_files/
+cp -r ./data_pipelines/vaccine_data_files/ ./docs/data_pipelines/vaccine_data_files/
  
 if [[ "$(git status --porcelain)" != "" ]]; then
     git config --global user.name 'RamiKrispin'
