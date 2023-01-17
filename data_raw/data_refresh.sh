dfb36b275a77700101ba2529c4095e8b3798aac9
rkrispin <rkrispin@apple.com>
2023-01-17 02:25:17 -0800

    update the data refresh process

--- data_raw/data_refresh.sh
+++ data_raw/data_refresh.sh
@@ -2,13 +2,9 @@
 
 echo "Updating the total dataset"
 
-quarto render ./data_pipelines/covid19_cases.qmd --to html  
-cp ./data_pipelines/covid19_cases.html ./docs/data_pipelines/
-cp -r ./data_pipelines/covid19_cases_files/ ./docs/data_pipelines/
-quarto render ./data_pipelines/vaccine_data.qmd --to html 
-cp ./data_pipelines/vaccine_data.html ./docs/data_pipelines/vaccine_data.html
-cp -r ./data_pipelines/vaccine_data_files/ ./docs/data_pipelines/vaccine_data_files/
- 
+quarto render ./data_pipelines/covid19_cases.qmd --to html
+quarto render ./data_pipelines/vaccine_data.qmd --to html
+
 if [[ "$(git status --porcelain)" != "" ]]; then
     git config --global user.name 'RamiKrispin'
     git config --global user.email 'ramkrisp@umich.edu'
