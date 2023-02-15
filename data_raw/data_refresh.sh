b8777158af02121d49c1d70b5f9858aa56769022
rkrispin <rkrispin@apple.com>
2023-02-14 21:40:29 -0800

    updated the rendered docs -> copy to the docs path

--- data_raw/data_refresh.sh
+++ data_raw/data_refresh.sh
@@ -4,6 +4,15 @@ echo "Updating the total dataset"
 
 quarto render ./data_pipelines/covid19_cases.qmd --to html
 quarto render ./data_pipelines/vaccine_data.qmd --to html
+cp ./data_pipelines/covid19_cases.html ./docs/data_pipelines/
+cp ./data_pipelines/vaccine_data.html ./docs/data_pipelines/
+
+rm -rf ./docs/data_pipelines/covid19_cases_files/
+cp -R ./data_pipelines/covid19_cases_files/ ./docs/data_pipelines/covid19_cases_files/
+
+rm -rf ./docs/data_pipelines/vaccine_data_files/
+cp -R ./data_pipelines/vaccine_data_files/ ./docs/data_pipelines/vaccine_data_files
+
 
 if [[ "$(git status --porcelain)" != "" ]]; then
     git config --global user.name 'RamiKrispin'
